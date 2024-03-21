e3d2611fa686ff365382c5cda950dc361d5604a8
poryos <poh.yossawin@gmail.com>
2024-03-21 11:19:53 +0700

    d

---
+++ Assets/Scripts/SceneChange.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneChange : MonoBehaviour
+{
+    public string SampleScene;
+    
+    public void LoadScene()
+    {
+        SceneManager.LoadScene(SampleScene);
+    }
+  
+}
