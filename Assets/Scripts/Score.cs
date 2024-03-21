5f25fe6e324558f627d272240f632c158be68cbf
NawapolNantajul <nawapol_nantajul@hotmail.com>
2024-03-21 14:46:19 +0700

    Score + UI + Sound and Background
    เยอะหน่อยนะ

---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Score : MonoBehaviour
+{
+    public int score;
+    public Text scoreText;
+    private AudioSource audioSource;
+    public AudioClip itemSound;
+    void Start()
+    {
+        score = 0;
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Items")
+        {
+            score++;
+            scoreText.text = "SCORE = " + score;
+            Destroy(other.gameObject);
+            audioSource.PlayOneShot(itemSound);
+        }
+    }
+}
